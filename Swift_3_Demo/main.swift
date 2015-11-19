3aaf9dfce062f0dfbdd7a7499978a6809a9d7e1d
傅昭瑞 <Jiayu_Zachary@zacharys-macbook-pro.local>
2015-11-19 15:42:21 +0800

    Initial Commit

---
+++ Swift_3_Demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_3_Demo
+//
+//  Created by Jiayu_Zachary on 15/11/19.
+//  Copyright © 2015年 Zachary. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
